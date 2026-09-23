b39872356b98ba94795248771c69f1ecdc3a3134
fabricio-pilla <195329461+fabricio-pilla@users.noreply.github.com>
2026-09-23 09:02:35 -0300

    O usuário reportou dois problemas/pedidos no sistema de gestão financei…

---
+++ pocketbase/migrations/0047_update_users_rules.js
@@ -0,0 +1,14 @@
+migrate(
+  (app) => {
+    const users = app.findCollectionByNameOrId('_pb_users_auth_')
+    users.viewRule = "@request.auth.id != ''"
+    users.updateRule = "@request.auth.id != ''"
+    app.save(users)
+  },
+  (app) => {
+    const users = app.findCollectionByNameOrId('_pb_users_auth_')
+    users.viewRule = 'id = @request.auth.id'
+    users.updateRule = 'id = @request.auth.id'
+    app.save(users)
+  },
+)
